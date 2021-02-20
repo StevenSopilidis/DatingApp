bc46387f28e738dac562491b0e8252fe4d595a49
StevenSopilidis <sopilidimaria@gmail.com>
2021-02-20 17:15:49 +0200

    fixing problem with postgres connection string

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -38,28 +38,23 @@ namespace API.Extensions
                 else
                 {
                     // Use connection string provided at runtime by Heroku.
-                    var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+                    string envVar = Environment.GetEnvironmentVariable("DATABASE_URL");
 
                     // Parse connection URL to connection string for Npgsql
-                    connUrl = connUrl.Replace("postgres://", string.Empty);
-                    var pgUserPass = connUrl.Split("@")[0];
-                    var pgHostPortDb = connUrl.Split("@")[1];
-                    var pgHostPort = pgHostPortDb.Split("/")[0];
-                    var pgDb = pgHostPortDb.Split("/")[1];
-                    var pgUser = pgUserPass.Split(":")[0];
-                    var pgPass = pgUserPass.Split(":")[1];
-                    var pgHost = pgHostPort.Split(":")[0];
-                    var pgPort = pgHostPort.Split(":")[1];
-
-                    connStr = $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}";
+                    var uri = new Uri(envVar);
+                    var username = uri.UserInfo.Split(':')[0];
+                    var password = uri.UserInfo.Split(':')[1];
+                    connStr = 
+                    "; Database=" + uri.AbsolutePath.Substring(1) +
+                    "; Username=" + username +
+                    "; Password=" + password + 
+                    "; Port=" + uri.Port +
+                    "; SSL Mode=Require; Trust Server Certificate=true;";
                 }
-
                 // Whether the connection string came from the local development configuration file
                 // or from the environment variable from Heroku, use it to set up your DbContext.
                 options.UseNpgsql(connStr);
             });
-
-
             return services;
         }
     }
